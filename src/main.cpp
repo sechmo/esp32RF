19908eed2dd4785d6033870b877230240e3675a9
sechmo <sesanchezmo@unal.edu.co>
2024-09-18 05:06:33 -0500

    leds

--- src/main.cpp
+++ src/main.cpp
@@ -1,223 +1,54 @@
-// ask_receiver.pde
-// -*- mode: C++ -*-
-// Simple example of how to use RadioHead to receive messages
-// with a simple ASK transmitter in a very simple way.
-// Implements a simplex (one-way) receiver with an Rx-B1 module
-// Tested on Arduino Mega, Duemilanova, Uno, Due, Teensy, ESP-12
-
 #include <Arduino.h>
-// #include <SPIFFS.h>
 
 
-#define isReceiver true
+#define isReceiver false
 #define USE_RADIO 1 
-// const bool isReceiver = true;
 
-// const char *startFilePath="/";
-// const char* ext="wav";
-// AudioSourceSPIFFS source(startFilePath, ext);
-// WAVDecoder decoder;
+const uint8_t inputPin = GPIO_NUM_14;
+const uint8_t outputPin = GPIO_NUM_32;
+const int ledInternal = GPIO_NUM_2;
+const int ledPin0 = GPIO_NUM_23; // amarillo
+const int ledPin1 = GPIO_NUM_22; // mroado
+const int ledPin2 = GPIO_NUM_21; // naranja
+const int ledPin3 = GPIO_NUM_19; // blanco
 
-
-// FileLoop floop;
-// EncodedAudioStream decoded(&floop, new WAVDecoder());
-
-// AudioWAVServer server("_", "_", 3333);
-// AnalogAudioStream in;
-
-// StreamCopy copier(radio, sound);
-
-
-
-// CsvOutput<int16_t> csv(Serial);
-// EncodedAudioStream wav(&csv, new WAVEncoder());
-
-// ConverterFillLeftAndRight<int16_t> filler(LeftIsEmpty); // fill both channels - or change to RightIsEmpty
-
-
-// StreamCopy copier(out, sound);
-// StreamCopy copier(out, floop);
-// StreamCopy copier(wav, in);
-
-
-// AudioPlayer player(source,out, decoder);
-// void printMetaData(MetaDataType type, const char* str, int len){
-//   Serial.print("==> ");
-//   Serial.print(toStr(type));
-//   Serial.print(": ");
-//   Serial.println(str);
-// }
-
-
-
-
-#if (USE_RADIO == 1)
-const uint8_t inputPin = GPIO_NUM_33;
-const uint8_t outputPin = GPIO_NUM_19;
-const int ledPin = GPIO_NUM_2;
-
-const int speed = 2000;
+const int speed = 3000;
 #define USE_OWN 1
-#if (USE_OWN == 1)
 #include <RadioCoder.h>
 
 RadioCoder driver(speed, inputPin, outputPin, 0); // ESP8266 or ESP32: do not use pin 11 or 2
-#else
-
-#include <RH_ASK.h>
-#include <SPI.h> // Not actually used but needed to compile
-RH_ASK driver(speed, inputPin, outputPin, 0); // ESP8266 or ESP32: do not use pin 11 or 2
-#endif
 
 
 uint8_t rxBufLen;
 uint8_t* rxBuf;
-#else 
-
-
-#include "AudioTools.h"
-// #include  <AudioLibs/AudioSourceSPIFFS.h>
-// #include  <AudioLibs/FileLoop.h>
-#include "RadioStream.h"
-
-AudioInfo info(2000, 1, 16);
-SineWaveGenerator<int16_t> wave(16000);
-GeneratedSoundStream<int16_t> sound(wave);
-RadioStream radio;
-AnalogAudioStream out;
-#if (isReceiver)
-StreamCopy copier(out,radio);
-#else
-StreamCopy copier(radio,sound);
-#endif
-
-#endif
 
 void setup()
 {
     Serial.begin(115200); // Debugging only
 
-    #if (USE_RADIO == 1)
     if (!driver.init())
         Serial.println("init failed");
-    pinMode(ledPin, OUTPUT);
+    
+    pinMode(ledInternal, OUTPUT);
+    pinMode(ledPin0, OUTPUT);
+    pinMode(ledPin1, OUTPUT);
+    pinMode(ledPin2, OUTPUT);
+    pinMode(ledPin3, OUTPUT);
 
 
     rxBufLen = driver.maxMessageLength();
 
     rxBuf = new uint8_t[rxBufLen];
-    #else
 
-
-    // SPIFFS.begin();
-
-    // File f = SPIFFS.open("/sample.wav", "r");
-
-    // floop.setFile(f);
-    // floop.begin();
-
-    // decoded.begin();
-
-    AudioLogger::instance().begin(Serial, AudioLogger::Debug);
-
-
-    if (!radio.begin())
-    {
-        Serial.println("radio begin failed");
-    }
-
-#if (!isReceiver)
-    sound.begin(info);
-    wave.begin(info, N_B4);
-#else
-    auto outConfig = out.defaultConfig(TX_MODE);
-    outConfig.copyFrom(info);
-    out.begin(outConfig);
-#endif
+    Serial.println("setup done");
 
 
 
-    copier.begin(); 
-
-
-
-
-
-#endif
-
-
-
-
-
-
-
-
-    // auto inConfig = in.defaultConfig(RX_MODE);
-    // inConfig.copyFrom(info);
-    // in.begin(inConfig);
-
-    // wav.begin();
-
-    // wave.begin(info, N_B4);
-
-    // source.setFileFilter("*.wav");
-    // player.setMetadataCallback(printMetaData);
-
-    // player.begin();
-
-    // copier.begin();
-
-    // server.begin(in, inConfig, nullptr);
-
-
-        
-    // Serial.println("setup done");
-}
-#if (USE_RADIO == 1)
-
-
-
-int available() {
-
-    // LOGI("Available: %d", driver->available());
-    // LOGI("Ramp: %d", driver->getRamp());
-    // return driver->availableLength();
-
-    rxBufLen = driver.maxMessageLength();
-
-    bool hasMsg  = driver.recv(rxBuf, &rxBufLen) ;
-
-    return hasMsg ? rxBufLen: 0;
-}
-
-size_t readBytes(uint8_t* data, size_t len)  {
-
-
-
-    // if (!driver->available()) {
-    //     return 0;
-    // }
-
-
-    uint8_t len8 = len < 0 ? 0 : (len > rxBufLen ? rxBufLen : len);
-
-    memcpy(data, rxBuf, len8);
-
-    // if (!driver->recv(data, &len8)) {
-    //     LOGE("Failed to receive message");
-    //     return 0;
-    // }
-
-    rxBufLen = driver.maxMessageLength();
-
-    return len8;
 }
 
 
 void loopReceiver()
 {
-    int16_t *data = (int16_t *)rxBuf;
-    // int bytes = available();
 
     if (driver.recv(rxBuf, &rxBufLen))
     {
@@ -228,14 +59,45 @@ void loopReceiver()
         //     Serial.print((char)buf[i]);
         // }
         // Serial.println();
-        for (int i = 0; i < rxBufLen/sizeof(int16_t); i++)
+        for (int i = 0; i < rxBufLen; i++)
         {
-            Serial.print(data[i]);
+            Serial.print(rxBuf[i]);
         }
         Serial.println();
-        // buflen = driver.maxMessageLength(); // Reset the length for the next message
-    }
 
+        digitalWrite(ledInternal, HIGH);
+        delay(50);
+        digitalWrite(ledInternal, LOW);
+        delay(50);
+        rxBufLen = driver.maxMessageLength(); // Reset the length for the next message
+    }
+    // int16_t *data = (int16_t *)rxBuf;
+    // // int bytes = available();
+
+    // if (driver.recv(rxBuf, &rxBufLen))
+    // {
+    //     // Message with a good checksum received, dump it.
+    //     // Serial.print("Got: ");
+    //     // for (int i = 0; i < buflen; i++)
+    //     // {
+    //     //     Serial.print((char)buf[i]);
+    //     // }
+    //     // Serial.println();
+    //     for (int i = 0; i < rxBufLen/sizeof(int16_t); i++)
+    //     {
+    //         Serial.print(data[i]);
+    //     }
+    //     Serial.println();
+
+    //     digitalWrite(ledPin, HIGH);
+    //     delay(50);
+    //     digitalWrite(ledPin, LOW);
+    //     delay(50);
+    //     // buflen = driver.maxMessageLength(); // Reset the length for the next message
+    // }
+
+
+// THIS works
 
     // int16_t data[1024];
     // size_t dataLen = 1024;
@@ -249,106 +111,73 @@ void loopReceiver()
 
     // // Serial.println("here");
 
-    // if (hasMsg) // Non-blocking
+    // if (dataLen > 0) // Non-blocking
     // {
 
-    //     // uint8_t len8 = dataLen < 0 ? 0 : (dataLen > rxBufLen ? rxBufLen : dataLen);
-
-    //     memcpy(data, rxBuf, dataLen);
-
+    //     uint8_t len8 = dataLen < 0 ? 0 : (dataLen > rxBufLen ? rxBufLen : dataLen);
     //     // dataLen = len8;
+
+    //     memcpy(data, rxBuf, len8);
+
 
     //     int i;
 
-    //     // Message with a good checksum received, dump it.
-    //     // Serial.print("Got: ");
-    //     // for (int i = 0; i < buflen; i++)
-    //     // {
-    //     //     Serial.print((char)buf[i]);
-    //     // }
-    //     // Serial.println();
     //     for (int i = 0; i < dataLen / sizeof(int16_t); i++)
     //     {
+    //         Serial.printf("%d:",i);
     //         Serial.println(data[i]);
-    //         // Serial.print("");
     //     }
 
     //     digitalWrite(ledPin, HIGH);
-    //     delay(50);
+    //     delay(6);
     //     digitalWrite(ledPin, LOW);
-    //     delay(50);
+    //     delay(6);
     //     // buflen = driver.maxMessageLength(); // Reset the length for the next message
     // }
     // dataLen = 1024;
 
-    // int bytes = available();
-    // int16_t data[1024];
-    // uint8_t dataLen = 1024;
+// --------
 
-    // // Serial.println(bytes);
-    // if (bytes > 0)
+/// Also works
+
+    // int16_t data[1024];
+    // size_t dataLen = 1024;
+
+    // // uint8_t bl = driver.maxMessageLength();
+    // dataLen = available();
+
+    // // Serial.println("here");
+
+    // if (dataLen > 0) // Non-blocking
     // {
-    //     readBytes((uint8_t *)data, bytes);
-    //     for (int i = 0; i < bytes / sizeof(int16_t); i++)
+
+    //     dataLen = readBytes((uint8_t *)data, dataLen * sizeof(int16_t));
+
+    //     for (int i = 0; i < dataLen / sizeof(int16_t); i++)
     //     {
+    //         Serial.printf("%d:",i);
     //         Serial.println(data[i]);
     //     }
+
+    //     digitalWrite(ledPin, HIGH);
+    //     delay(6);
+    //     digitalWrite(ledPin, LOW);
+    //     delay(6);
+
     // }
+
+////////////////////
 }
 void loopTransmitter()
 {
 
-    const char *msg = "hello";
-    driver.send((uint8_t *)msg, strlen(msg));
+    const char* data = "Hello, world!";
+
+    driver.send((uint8_t*)data, strlen(data));
     driver.waitPacketSent();
 
-    Serial.print("Sent");
-    digitalWrite(ledPin, HIGH);
-    delay(200);
-    digitalWrite(ledPin, LOW);
-    delay(200);
-
-    // digitalWrite(ledPin, LOW);
-    // delay(2000);
-
-    // // read the serial input and transmit it
-    // if (Serial.available())
-    // {
-    //     // Read the serial data
-    //     char data = Serial.read();
-    //     // Send the data
-    //     driver.send((uint8_t *)&data, 1);
-    //     driver.waitPacketSent();
-    //     // Print the data to the serial monitor
-    //     Serial.print("Sent: ");
-    //     Serial.println(data);
-    // }
+    Serial.println("Sent");
 }
-#else 
-
-void loopReceiver()
-{
-    // copier.copy();
-    int bytes = radio.available();
-    int16_t data[1024];
-
-    Serial.println(bytes);
-    if (bytes > 0)
-    {
-        radio.readBytes((uint8_t *)data, bytes);
-        for (int i = 0; i < bytes / sizeof(int16_t); i++)
-        {
-            Serial.println(data[i]);
-        }
-    }
-}
-
-void loopTransmitter()
-{
-    copier.copy();
-}
-#endif
-
 void loop()
 {
 
@@ -360,5 +189,19 @@ void loop()
     {
         loopTransmitter();
     }
+
+    // digitalWrite(ledPin0, HIGH);
+    // delay(200);
+    // digitalWrite(ledPin0, LOW);
+    // digitalWrite(ledPin1, HIGH);
+    // delay(200);
+    // digitalWrite(ledPin1, LOW);
+    // digitalWrite(ledPin2, HIGH);
+    // delay(200);
+    // digitalWrite(ledPin2, LOW);
+    // digitalWrite(ledPin3, HIGH);
+    // delay(200);
+    // digitalWrite(ledPin3, LOW);
+    // delay(200);
 
 }
